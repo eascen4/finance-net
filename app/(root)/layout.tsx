aa575962c568416ac0ef722078a5cada52b697bb
Elias Ascencio <128012388+revenge789@users.noreply.github.com>
2024-05-09 18:45:26 -0500

    chore: Update npm dependencies
    This commit updates the npm dependencies in the `package.json` file. It adds the following packages:
    - `@radix-ui/react-dialog` version 1.0.5
    - `chart.js` version 4.4.2
    - `query-string` version 9.0.0
    - `react-chartjs-2` version 5.2.0
    - `react-countup` version 6.5.3
    
    These updates are necessary to ensure compatibility and take advantage of the latest features and bug fixes provided by these packages.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,31 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Elias", lastName: "Ascencio" };
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex flex-col size-full">
+        <div className="root-layout">
+          <Image
+            src="/icons/logo.svg"
+            alt="logo"
+            width={30}
+            height={30}
+          />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+      {children}
+      </div>
     </main>
   );
 }
